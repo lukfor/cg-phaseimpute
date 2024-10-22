7403b71b792c07f6d0fae2d40a3c489f2c18c9e0
Lukas Forer <lukas.forer@i-med.ac.at>
2024-10-22 13:49:49 +0200

    Init commit

---
+++ bin/create_samplesheet.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Check if correct number of arguments is provided
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <input_directory> <output_csv>"
+  exit 1
+fi
+
+# Command-line arguments
+input_dir="$1"
+output_csv="$2"
+
+# Create the header of the CSV file
+echo "sample,file,index" > "$output_csv"
+
+# Loop through all BAM files in the directory
+for bam_file in "$input_dir"/*.bam; do
+  # Get the base filename without the extension
+  sample_name=$(basename "$bam_file" .bam | sed 's/\./_/g')
+  
+  # Find the corresponding BAI file
+  bai_file="${bam_file}.bai"
+  echo "index file found for $bai_file"
+  # If the BAI file exists, add the entry to the CSV
+  if [ -f "$bai_file" ]; then
+    echo "$sample_name,$bam_file,$bai_file" >> "$output_csv"
+  else
+    echo "Warning: No index file found for $bam_file"
+  fi
+done
+
+echo "CSV file created: $output_csv"
